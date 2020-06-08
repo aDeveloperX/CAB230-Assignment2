42abfa38febca3cf0c84c6e35eb7b24eeaa6716c
aDeveloperX <w369371@gmail.com>
2020-06-08 15:22:31 +1000

    change logger

--- app.js
+++ app.js
@@ -27,7 +27,7 @@ app.use(cors());
 app.set("views", path.join(__dirname, "views"));
 app.set("view engine", "jade");
 
-app.use(logger("dev"));
+app.use(logger("common"));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
